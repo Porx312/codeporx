19146db66121383e315e48f6fb9d31959fa9d389
porx <105926826+Porx312@users.noreply.github.com>
2025-01-16 19:51:12 +0100

    'xd'

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Articulos' },
-  { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projectos' },
 ]
 
