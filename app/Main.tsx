82c45226c02e5322c163e7f2e3444d20948336e9
Jose <gregoriowhite333@gmail.com>
2025-07-14 09:57:25 +0200

    xd

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,12 +1,4 @@
-import HomeMain from '@/components/home/Home'
-import SkillsSlider from '@/components/home/Skills'
-import Project from '@/components/home/Projects'
-import StudyTimeline from '@/components/home/StudyTimeLine'
-import AboutMe from '@/components/home/Aboutme'
-import Contact from '@/components/new/Contact'
 import StickyMenu from '@/components/new/MenuSticky'
-import ProjectCardBanner from '@/components/home/idebanner'
-import Skills from '@/components/new/Skills'
 import Portfolio from '@/components/new/portfolio'
 
 export default function Home() {
